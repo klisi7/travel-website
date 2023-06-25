14b8adef7f52278a3215d7791bdd1d47a2537deb
klisi7 <117843390+klisi7@users.noreply.github.com>
2023-06-25 22:28:04 +0200

    Update cikkek.js

--- cikkek.js
+++ cikkek.js
@@ -1,3 +1,5 @@
+
+
 var nav = document.querySelector("nav");
 
 document.querySelector(".toggle").onclick = () =>{
@@ -17,4 +19,37 @@ window.addEventListener("scroll", () =>{
     let scrolled = window.scrollY;
 
     nav.classList.toggle("sticky", scrolled > 10);
+})
+
+if(document.querySelector(".header_btn") == undefined){
+    document.querySelectorAll(".nav_links").forEach(links =>{
+        links.style.color = "#ef233c";
+        links.style.fontWeight = "100";
+        links.style.boxShadow = "none";
+    })
+}
+
+
+let selectBtn = document.querySelector(".select_btn"),
+    items = document.querySelectorAll(".item");
+
+selectBtn.onclick = () =>{
+    selectBtn.classList.toggle("open");
+}
+
+items.forEach(target =>{
+    let newBtn = document.createElement("button");
+            newBtn.classList.add("go_btn");
+            newBtn.innerHTML = "Keres";
+
+    target.onclick = () =>{
+        target.classList.toggle("checked");
+
+        let checked = document.querySelectorAll(".checked");
+        let checkedTxt = target.querySelector(".item_text").innerHTML;
+
+        if(checked){
+            document.querySelector(".select_container").appendChild(newBtn);
+        }
+    }
 })
